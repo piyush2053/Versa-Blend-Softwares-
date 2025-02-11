b1e1ec519073fd39966603e9add1f7561a7c09e8
piyush.c.patel <piyush.patel@rws.com>
2025-02-11 23:36:16 +0530

    feat: Fixed globe placement

--- src/Components/Chunks/Globe/Globe.tsx
+++ src/Components/Chunks/Globe/Globe.tsx
@@ -3,7 +3,7 @@ import gifWorld from '../../../Assets/Gif/world.gif';
 const GlobeComponent = () => {
   return (
     <div className="bg-black flex flex-col md:flex-row items-center md:items-start text-center md:text-left p-4">
-      <img src={gifWorld} className='w-50 h-40' alt='Globe_PP' />
+      <img src={gifWorld} className='w-50 h-40 my-auto' alt='Globe_PP' />
       <div className="gap-2 md:ml-4 mt-4 md:mt-10">
         <p className="text-[45px] text-white font-thin text-opacity-90">Providing Services Around the Globe</p>
         <p className="text-lg mt-2 text-white max-w-2xl text-opacity-60">
